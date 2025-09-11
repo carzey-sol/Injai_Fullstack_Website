91a82dff420ba5ae872e6445fe2cff127cf6a880
Mukesh Bajgain <bajgainmukesh73@gmail.com>
2025-09-11 12:01:24 +0545

    Patch 1.3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.ytimg.com', 'localhost'],
+    domains: ['i.ytimg.com', 'localhost', 'res.cloudinary.com'],
   },
 }
 
